5c6e64cea5a1d86f8d8fcf0cba57e999b588bcfe
Silviya Guzen <sis.kuneva@gmail.com>
2025-05-27 12:43:06 +0300

    x fixed Application proj

---
+++ src/IAMS.Application/Interfaces/IPolicyPaymentRepository.cs
@@ -0,0 +1,13 @@
+﻿using IAMS.Domain.Entities;
+
+
+namespace IAMS.Application.Interfaces
+{
+    public interface IPolicyPaymentRepository : IRepository<PolicyPayment>
+    {
+        Task<IEnumerable<PolicyPayment>> GetPaymentsByPolicyIdAsync(int policyId);
+        Task<IEnumerable<PolicyPayment>> GetOverduePaymentsAsync();
+        Task<IEnumerable<PolicyPayment>> GetPaymentsByDateRangeAsync(DateTime fromDate, DateTime toDate);
+        Task<decimal> GetTotalPaymentsByPolicyIdAsync(int policyId);
+    }
+}
--- src/IAMS.Persistence/obj/Debug/net8.0/IAMS.Persistence.AssemblyInfo.cs
+++ src/IAMS.Persistence/obj/Debug/net8.0/IAMS.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IAMS.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2223d8b66adae2a6e02ca03d9727492ad9f6ea8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271372f04103dbcd44160dc08a123d2c7711c048")]
 [assembly: System.Reflection.AssemblyProductAttribute("IAMS.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IAMS.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
